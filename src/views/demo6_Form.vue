8df4e7c1138ab962d2874981c0ad2f19c36c639a
RyosakiZ <59913098+RyosakiZ@users.noreply.github.com>
2020-04-13 17:07:31 +0700

    added modifiers

--- src/views/demo6_Form.vue
+++ src/views/demo6_Form.vue
@@ -3,24 +3,31 @@
     <form style="margin-top:20px" v-on:submit.prevent="submit">
       <!-- Firstname -->
       <div>
-        <label for="first_name">Firstname</label>
-        <input type="text" name="first_name" v-model="applicant.first_name" />
+        <label for="first_name">Firstname: </label>
+        <input type="text" name="first_name" v-model.trim="applicant.first_name" />
       </div>
       <!-- Lastname -->
       <div>
-        <label for="last_name">Lastname</label>
-        <input type="text" name="last_name" v-model="applicant.last_name" />
+        <!-- lazy only done when user focus other box -->
+        <label for="last_name">Lastname: </label>
+        <input type="text" name="last_name" v-model.trim="applicant.last_name" />
       </div>
       <!-- Sex -->
       <div>
+        Sex: 
         <label for="sex">Male</label>
         <input type="radio" name="sex" value="male" v-model="applicant.sex" />
         <label for="sex">Female</label>
         <input type="radio" name="sex" value="female" v-model="applicant.sex" />
       </div>
+      <!-- Age -->
+      <div>
+        <label for="age">Age: </label>
+        <input type="number" v-model.number="applicant.age">
+      </div>
       <!-- Courses -->
       <div>
-        Course:
+        Course: 
         <!-- name must be matches with data, otherwise it will not autochoose -->
         <select name="courses" v-model="applicant.courses"> 
           <option value="react">React</option>
@@ -29,7 +36,7 @@
         </select>
       </div>
 
-      <button type="button" @click="clear">Clear</button> 
+      <button type="button" @click.exact="clear" @click.alt="useDefault">Clear</button> 
       <button type="submit">Submit</button>
       
     </form>
@@ -49,6 +56,7 @@ export default {
         first_name: "",
         last_name: "",
         sex: "male",
+        age: 18,
         courses: "react"
       }
     };
@@ -63,6 +71,16 @@ export default {
         first_name: "",
         last_name: "",
         sex: "male",
+        age: 0,
+        courses: "react"
+      }
+    },
+    useDefault(){
+      this.applicant = {
+        first_name: "UNKNOWN",
+        last_name: "UNKNOWN",
+        sex: "male",
+        age: 10,
         courses: "react"
       }
     }
