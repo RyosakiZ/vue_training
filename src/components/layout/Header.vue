a86d1149bcc9d54bc0ca4e6ebef62b02f5c6adb3
RyosakiZ <59913098+RyosakiZ@users.noreply.github.com>
2020-04-13 17:30:35 +0700

    separated to components

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,7 +1,6 @@
 <template>
-  <div id="app">
-    <Header />
-    <!-- <ul>
+  <div id="header">
+    <ul>
       <li><router-link to="/"><button>Home</button></router-link></li>
       <li><router-link to="/demo1"><button>Demo1 Intro</button></router-link></li>
       <li><router-link to="/demo2"><button>Demo2 Events</button></router-link></li>
@@ -9,25 +8,18 @@
       <li><router-link to="/demo4"><button>Demo4 Compute</button></router-link></li>
       <li><router-link to="/demo5"><button>Demo5 Binding</button></router-link></li>
       <li><router-link to="/demo6"><button>Demo6 Form & V-Model</button></router-link></li>
-    </ul> -->
-
-    <router-view></router-view>
-    <span class="footer">Copyright 2020 by Puwit Chotikasatian</span>
+    </ul>
   </div>
 </template>
 
 <script>
-import Header from "@/components/layout/Header.vue"
-
 export default {
-  name: "app",
-  components: {
-    Header
-  }
-};
+    name: "Header"
+}
 </script>
 
 <style scoped>
+
 ul {
   margin: 0;
   padding: 0;
@@ -37,14 +29,4 @@ li {
   margin-right: 8px;
 }
 
-.title{
-  font-size: 15px;
-  color: grey;
-}
-</style>
-
-<style>
-.content{
-  color:blue;
-}
 </style>
